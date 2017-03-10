93da8de34c42f5fc9ef82e673575fbde8d90d14a
ssaczkows <ssaczkows@outlook.com>
2017-03-09 22:22:21 -0300

    Add AsyncTask for ParseJson

--- app/src/main/java/com/ses/pc/radarapp/MapsActivity.java
+++ app/src/main/java/com/ses/pc/radarapp/MapsActivity.java
@@ -3,6 +3,7 @@ package com.ses.pc.radarapp;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,14 +15,19 @@ import com.google.gson.Gson;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private final static String ADDS_ENEMY="https://redarmyserver.appspot.com/_ah/api/myApi/v1/torretinfocollection";
+    private final static String ADDS_ENEMY = "https://redarmyserver.appspot.com/_ah/api/myApi/v1/torretinfocollection";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager()
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
@@ -32,21 +38,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
         try {
-            mMap=googleMap;
-            Location location=this.getLocalizationWeapon();
+            mMap = googleMap;
+            Location location = this.getLocalizationWeapon();
 
-
-            System.out.println("SASASASASASAA" + location.getLatitude());
-            System.out.println("SASASASASASAA" + location.getLongitude());
-
-
-            LatLng weaponLocation=new LatLng(location.getLatitude(), location.getLongitude());
+            LatLng weaponLocation = new LatLng(location.getLatitude(), location.getLongitude());
 
             mMap.addMarker(new MarkerOptions().position(weaponLocation).title("misil"));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(weaponLocation));
 
         } catch (Exception e) {
-            System.out.println("MENSAJEEEEEE:"+e.getMessage());
+            e.getStackTrace();
         }
 
     }
@@ -54,19 +55,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public Location getLocalizationWeapon() throws Exception {
 
 
-        Location location=new Location();
-        ParseJson parse=new ParseJson();
-        Gson gson=new Gson();
+        Location location = new Location();
+        ParseJson parse = new ParseJson();
+        Gson gson = new Gson();
 
-        String json=parse.readUrl(ADDS_ENEMY);
+        String json = parse.readUrl(ADDS_ENEMY);
 
-        EnemyMessageJson msg=gson.fromJson(json, EnemyMessageJson.class);
+        EnemyMessageJson msg = gson.fromJson(json, EnemyMessageJson.class);
 
         location.setLatitude(msg.getItems().get(0).getLocation().getLatitude());
         location.setLongitude(msg.getItems().get(0).getLocation().getLongitude());
 
         return location;
     }
-
 
 }
--- app/src/main/java/com/ses/pc/radarapp/ParseJson.java
+++ app/src/main/java/com/ses/pc/radarapp/ParseJson.java
@@ -1,31 +1,67 @@
 package com.ses.pc.radarapp;
 
+import android.os.AsyncTask;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
-/**
- * Created by gescobar on 3/1/17.
- */
 
 public class ParseJson {
 
-    public String readUrl(String urlString) throws Exception {
-        BufferedReader reader = null;
+    public String readUrl(String urlString) {
+
+        AsyncTask<String, Void, String> task = new AsyncConector().execute(urlString);
+
         try {
-            URL url = new URL(urlString);
+            return task.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
+
+class AsyncConector extends AsyncTask<String, Void, String> {
+
+    String dataJson = new String();
+
+
+    @Override
+    protected String doInBackground(String... urlString) {
+        BufferedReader reader = null;
+
+        try {
+            URL url = new URL(urlString[0]);
+
             reader = new BufferedReader(new InputStreamReader(url.openStream()));
+
             StringBuffer buffer = new StringBuffer();
             int read;
             char[] chars = new char[1024];
             while ((read = reader.read(chars)) != -1)
                 buffer.append(chars, 0, read);
 
-            return buffer.toString();
+            dataJson = buffer.toString();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         } finally {
             if (reader != null)
-                reader.close();
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
         }
+        return dataJson;
     }
 
+    public String getData() {
+        return dataJson;
+    }
 }
