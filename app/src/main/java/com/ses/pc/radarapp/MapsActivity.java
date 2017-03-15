350ea37e63f9a20b82a219892365c6da80b6859e
gonzaloescobar <gonzalo.david.escobar@gmail.com>
2017-03-14 21:43:44 -0300

    New Feature: All Draw orange circles indicating

--- app/src/main/java/com/ses/pc/radarapp/MapsActivity.java
+++ app/src/main/java/com/ses/pc/radarapp/MapsActivity.java
@@ -20,7 +20,7 @@ import static com.ses.pc.radarapp.R.id.map;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private final static String ADDS_ENEMY = "https://redarmyserver.appspot.com/_ah/api/myApi/v1/torretinfocollection";
+    private final static String ADDS_ENEMY="https://redarmyserver.appspot.com/_ah/api/myApi/v1/torretinfocollection";
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -31,8 +31,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(map);
         mapFragment.getMapAsync(this);
     }
@@ -40,42 +39,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
-
         try {
-            mMap = googleMap;
-            EnemyMessageJson dataWeapon = this.getDataWeapon();
+            mMap=googleMap;
+            EnemyMessageJson dataWeapon=this.getDataWeapon();
 
-            LatLng weaponLocation = new LatLng(dataWeapon.getItems().get(0).getLocation().getLatitude(), dataWeapon.getItems().get(0).getLocation().getLongitude());
-            mMap.addCircle(new CircleOptions()
-                    .center(new LatLng(dataWeapon.getItems().get(0).getLocation().getLatitude(), dataWeapon.getItems().get(0).getLocation().getLongitude()))
-                    .radius(dataWeapon.getItems().get(0).getRadius())
-                    .strokeColor(Color.RED)
-                    .fillColor(Color.rgb(255, 117, 020)));
-            mMap.addMarker(new MarkerOptions().position(weaponLocation).title("misil"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(weaponLocation));
-
+            for (Item item : dataWeapon.getItems()) {
+                LatLng weaponLocation=new LatLng(item.getLocation().getLatitude(), item.getLocation().getLongitude());
+                mMap.addCircle(new CircleOptions().center(new LatLng(item.getLocation().getLatitude(), item.getLocation().getLongitude())).radius(item.getRadius()).strokeColor(Color.RED).fillColor(Color.rgb(255, 117, 020)));
+                mMap.addMarker(new MarkerOptions().position(weaponLocation).title("misil"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(weaponLocation));
+            }
         } catch (Exception e) {
             e.getStackTrace();
         }
-
     }
 
     public EnemyMessageJson getDataWeapon() throws Exception {
-
-
-        //Location location = new Location();
-        ParseJson parse = new ParseJson();
-        Gson gson = new Gson();
-
-        String json = parse.readUrl(ADDS_ENEMY);
-
-        EnemyMessageJson msg = gson.fromJson(json, EnemyMessageJson.class);
-
-        //location.setLatitude(msg.getItems().get(0).getLocation().getLatitude());
-        //location.setLongitude(msg.getItems().get(0).getLocation().getLongitude());
+        ParseJson parse=new ParseJson();
+        Gson gson=new Gson();
+        String json=parse.readUrl(ADDS_ENEMY);
+        EnemyMessageJson msg=gson.fromJson(json, EnemyMessageJson.class);
 
         return msg;
     }
-
 }
